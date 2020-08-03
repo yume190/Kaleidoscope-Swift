973e956ee8fd394da3a1093364205f85c98dd6db
Yume <yume190@gmail.com>
2020-08-03 15:17:24 +0800

    add some DI

--- Sources/Kaleidoscope/Command.swift
+++ Sources/Kaleidoscope/Command.swift
@@ -37,11 +37,54 @@ struct Command: ParsableCommand {
         }
         contexts.codeGen(input: code)
         
+        // MARK: Target & Feature
+        //* llvm-as < /dev/null | llc -march=x86 -mattr=help
         /// x86_64-apple-darwin19.5.0
-        let machine = try TargetMachine(triple: .default, cpu: "x86-64", features: "", optLevel: .default, relocations: .default, codeModel: .default)
+//        let cpu = "generic"
+        let cpu = "x86-64"
+        let features = ""
+        let machine = try TargetMachine(triple: .default, cpu: cpu, features: features, optLevel: .default, relocations: .default, codeModel: .default)
         contexts.module.targetTriple = .default
         contexts.module.dataLayout = machine.dataLayout
         contexts.passPipeliner.execute()
         try machine.emitToFile(module: contexts.module, type: .object, path: self.output)
     }
 }
+
+// struct DebugInfo {
+//     // DICompileUnit
+//     let cu: CompileUnitMetadata
+//     let dblTy: DIType?
+// //    DIType *getDoubleTy();
+//     var getDoubleTy: DIType {
+//         if let dblTy = dblTy {
+//             return dblTy
+//         }
+//         // DBuilder->createBasicType("double", 64, dwarf::DW_ATE_float);
+//         return DIBuilder(module: Module.init(name: "")).buildBasicType(named: "double", encoding: .float, flags: .accessibility, size: 64)
+//     }
+// }
+
+
+// func abc() {
+//     let builder = DIBuilder(module: Module.init(name: ""))
+    
+// //    DBuilder->createCompileUnit(
+// //        dwarf::DW_LANG_C,
+// //        DBuilder->createFile("fib.ks", "."),
+// //        "Kaleidoscope Compiler", producer: String
+// //        0, isOptimized: Bool ok
+// //        "", Flags: String ok
+// //        0); RV : unsigned ok runtime version
+//     builder.buildCompileUnit(
+//         for: .c,
+//         in: builder.buildFile(named: "fib.ks", in: "."),
+//         kind: .full // fulldebug
+//     )
+//     builder.finalize()
+    
+//     let di: DebugInfo? = nil
+//     di!.cu.file
+//     di!.cu.file?.directory
+//     // builder.buildFile(named: <#T##String#>, in: <#T##String#>)
+// }
